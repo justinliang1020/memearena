b559dbefc32527c40eee398598a8cfa86dacdafd
Justin Liang <justinliang1020@gmail.com>
2025-01-02 13:38:19 -0600

    initial dummy interface

---
+++ src/lib/WikipediaImageFigure.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	let { caption, imageSrc }: { caption: string; imageSrc: string } = $props();
+	//TODO: make another prop for light or dark mode
+</script>
+
+<figure>
+	<img src={imageSrc} alt="meow" />
+	<figcaption>
+		<svg xmlns="http://www.w3.org/2000/svg" width="15" height="11" viewBox="0 0 11 15">
+			<g id="magnify-clip" fill="#fff" stroke="#000">
+				<path id="bigbox" d="M1.509 1.865h10.99v7.919H1.509z" />
+				<path id="smallbox" d="M-1.499 6.868h5.943v4.904h-5.943z" />
+			</g>
+		</svg>
+		{@html caption}
+	</figcaption>
+</figure>
+
+<style>
+	figure {
+		width: 229px;
+		height: fit-content;
+		background-color: rgb(32, 33, 34);
+		border: 1px solid rgb(84, 89, 93);
+		border-bottom: 0;
+		font-family: sans-serif;
+		font-size: 16px;
+		display: table;
+		color: rgb(248, 249, 250);
+		position: relative;
+	}
+
+	figure img {
+		width: calc(100% - (2 * 3px) - (2 * 1px));
+		border: 1px solid #c8ccd1;
+		margin: 3px;
+		background-color: white;
+	}
+
+	figure figcaption {
+		position: relative;
+		display: table-caption;
+		caption-side: bottom;
+		line-height: 1.4em;
+		padding: 0 6px 6px 6px;
+		border: 1px solid rgb(84, 89, 93);
+		border-top: 0;
+		background-color: rgb(32, 33, 34);
+		font-size: 88%;
+		word-break: break-word;
+	}
+
+	figcaption svg {
+		float: right;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,165 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import WikipediaImageFigure from '$lib/WikipediaImageFigure.svelte';
+	interface Channel {
+		id: string;
+		name: string;
+		lastQueried: number; // ms from Date.now()
+		imageBlocks: ImageBlock[];
+		textBlocks: TextBlock[];
+	}
+	interface BlockBase {
+		id: string;
+		title: string;
+	}
+	interface ImageBlock extends BlockBase {
+		src: string;
+	}
+	interface TextBlock extends BlockBase {
+		content: string;
+	}
+	interface Meme {
+		imageBlock: ImageBlock;
+		textBlock: TextBlock;
+	}
+
+	let dummyChannel1: Channel = {
+		id: 'dummyChannelId1',
+		name: 'dummyChannelName1',
+		lastQueried: Date.now(),
+		imageBlocks: [
+			{
+				id: 'blockId1',
+				title: 'blockTitle1',
+				src: 'favicon.png'
+			}
+		],
+		textBlocks: [
+			{
+				id: 'blockId2',
+				title: 'blockTitle2',
+				content: 'blockContent2'
+			},
+			{
+				id: 'blockId5',
+				title: 'blockTitle5',
+				content: 'blockContent5'
+			}
+		]
+	};
+	let dummyChannel2: Channel = {
+		id: 'dummyChannelId2',
+		name: 'dummyChannelName2',
+		lastQueried: Date.now(),
+		imageBlocks: [
+			{
+				id: 'blockId3',
+				title: 'blockTitle3',
+				src: 'favicon.png'
+			},
+			{
+				id: 'blockId6',
+				title: 'blockTitle6',
+				src: 'favicon.png'
+			}
+		],
+		textBlocks: [
+			{
+				id: 'blockId4',
+				title: 'blockTitle4',
+				content: 'blockContent4'
+			}
+		]
+	};
+	let channels: Channel[] = $state([dummyChannel1, dummyChannel2]);
+	let dummyMeme1: Meme = {
+		imageBlock: dummyChannel1.imageBlocks[0],
+		textBlock: dummyChannel1.textBlocks[0]
+	};
+	let dummyMeme2: Meme = {
+		imageBlock: dummyChannel2.imageBlocks[0],
+		textBlock: dummyChannel2.textBlocks[0]
+	};
+	let memes: Meme[] = [
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme1,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2,
+		dummyMeme2
+	];
+</script>
+
+<main>
+	<section class="sidebar">
+		<h1>memeare.na</h1>
+		<p>Randomly generated memes curated from <a href="https://www.are.na/">are.na</a> channels</p>
+		<h2>Channels</h2>
+		<div style="text-align: left;">
+			<form action="">
+				<label for="channel-url">are.na Channel URL</label>
+				<input type="text" id="channel-url" />
+				<button>submit</button>
+			</form>
+			<ul class="channels-list">
+				{#each channels as channel}
+					<!-- content here -->
+					<li>
+						<a href="https://www.example.com/">{channel.name}</a>:
+						<b>{channel.imageBlocks.length}</b>
+						image blocks & <b>{channel.textBlocks.length}</b> text blocks
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</section>
+	<section class="content">
+		{#each memes as meme}
+			<WikipediaImageFigure caption={meme.textBlock.content} imageSrc={meme.imageBlock.src} />
+		{/each}
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		height: 100vh;
+	}
+
+	.channels-list {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	.sidebar {
+		flex-basis: 25rem;
+		flex-grow: 1;
+		text-align: center;
+		border-right: black dashed 5px;
+	}
+
+	.content {
+		flex-basis: 0;
+		flex-grow: 999;
+		min-inline-size: 50%;
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
