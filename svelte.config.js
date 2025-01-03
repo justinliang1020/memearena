44da76007a5b22b100ce77135d772468794ba63a
Justin Liang <justinliang1020@gmail.com>
2025-01-02 22:23:24 -0600

    github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+
+    // When building for GitHub Pages, if your repo name is not equivalent to your-username.github.io, 
+    // make sure to update config.kit.paths.base to match your repo name. 
+    // This is because the site will be served from https://your-username.github.io/your-repo-name rather than from the root.
+    // https://svelte.dev/docs/kit/adapter-static#GitHub-Pages
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
+  }
 };
 
 export default config;
